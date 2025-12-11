397108967b88be8ffe93e0b1e04d5dea7eadb697
Tavo <tavo1235@proton.me>
2025-12-11 15:18:02 -0500

    FIX: Ruta esplícita en los import con unpkg

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // --- 1. Imports y Configuración de Módulos ---
 
 import * as THREE from 'https://unpkg.com/three@0.137.0/build/three.module.js';
-import { GPUComputationRenderer } from 'https://cdn.skypack.dev/three@0.137.0/examples/jsm/misc/GPUComputationRenderer.js';
-import { EffectComposer } from 'https://cdn.skypack.dev/three@0.137.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://cdn.skypack.dev/three@0.137.0/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'https://cdn.skypack.dev/three@0.137.0/examples/jsm/postprocessing/UnrealBloomPass.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.137.0/examples/jsm/controls/OrbitControls.js';
-
+import { GPUComputationRenderer } from 'https://unpkg.com/three@0.137.0/examples/jsm/misc/GPUComputationRenderer.js';
+import { EffectComposer } from 'https://unpkg.com/three@0.137.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.137.0/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'https://unpkg.com/three@0.137.0/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.137.0/examples/jsm/controls/OrbitControls.js';
 // --- 2. Variables Globales y Setup Inicial --- 
 // ... el resto de tu código
 const N = 10000;
